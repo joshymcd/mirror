310f019133670f24afd6668588fdac537b604de5
Joshua McDonald <joshua.mcdonald@vision33.co.uk>
2023-09-15 16:37:51 +0100

    .

--- src/app/test/route.ts
+++ src/app/test/route.ts
@@ -3,5 +3,5 @@ import { NextResponse } from 'next/server'
  
 export async function POST(request: Request) {
   const res   = await request.json()   
-  return NextResponse.json(res)
+  return NextResponse.json({resp: res})
 } 
