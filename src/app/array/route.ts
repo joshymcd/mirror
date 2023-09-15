714975863409c8037e784033ea009daf2e050b7a
Joshua McDonald <joshua.mcdonald@vision33.co.uk>
2023-09-15 16:52:41 +0100

    dsa

--- src/app/array/route.ts
+++ src/app/array/route.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NextResponse } from 'next/server'
  
-export  function POST(request: Request) { 
+export async function POST(request: Request) { 
+    const res   = await request.text()  
+    console.log(res)
     const array = [{test: "hello"},{test: "hello"}]
   return NextResponse.json(array)
 } 
--- src/app/test/route.ts
+++ src/app/test/route.ts
@@ -3,5 +3,6 @@ import { NextResponse } from 'next/server'
  
 export async function POST(request: Request) {
   const res   = await request.text()  
+  console.log(res)
   return NextResponse.json({res: res})
 } 
